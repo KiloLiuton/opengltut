a6b5becb391b30fd8a3a96c2360e10dc01520876
Kevin Liu Rodrigues <kkevin.liu@protonmail.com>
2022-07-31 07:35:55 -0300

    night update

--- main.cpp
+++ main.cpp
@@ -155,7 +155,7 @@ int main(int argc, char** argv)
     // draw
     glm::mat4 Projection = glm::perspective(glm::radians(45.0f), (float) width / (float) height, 0.1f, 100.f); 
     glm::mat4 View = glm::lookAt(glm::vec3(4,3,3), glm::vec3(0,0,0), glm::vec3(0,1,0));
-    glm::mat4 Model = glm::mat4(1.0f);
+    glm::mat4 Model = glm::mat4(-10.0f);
     glm::mat4 mvp = Projection * View * Model;
     GLuint MatrixID = glGetUniformLocation(programID, "MVP");
     glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
